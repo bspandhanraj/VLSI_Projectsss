16664d95f05ab4ec32f63732d992a2fa6433ec6c
bspandhanraj <73156230+bspandhanraj@users.noreply.github.com>
2026-03-10 21:19:59 +0530

    Update design.sv

--- Half_Adder/design.sv
+++ Half_Adder/design.sv
@@ -1,1 +1,12 @@
+module half_adder (
+    input a,
+    input b,
+    output sum,
+    output carry
+);
 
+    // The logic for a Half-Adder
+    assign sum = a ^ b;   // XOR gate for the Sum
+    assign carry = a & b; // AND gate for the Carry
+
+endmodule
