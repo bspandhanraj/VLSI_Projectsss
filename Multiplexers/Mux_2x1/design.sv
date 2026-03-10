2282fedb57bc1518473e8383ee94145b9b5cc785
bspandhanraj <73156230+bspandhanraj@users.noreply.github.com>
2026-03-10 22:17:04 +0530

    Update design.sv

--- Multiplexers/Mux_2x1/design.sv
+++ Multiplexers/Mux_2x1/design.sv
@@ -1,1 +1,11 @@
+module mux_2x1 (
+    input d0,      // Data input 0
+    input d1,      // Data input 1
+    input sel,     // Selection line
+    output y       // Output
+);
 
+    // If sel is 1, y gets d1. If sel is 0, y gets d0.
+    assign y = sel ? d1 : d0; 
+
+endmodule
